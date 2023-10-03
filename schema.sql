a7f5a812d823308f5855867a03314c955feaf9c9
Pavlo Karas <kar.pavel123@gmail.com>
2023-10-03 22:22:39 +0300

    feat: add user data column

--- schema.sql
+++ schema.sql
@@ -1,30 +1,26 @@
 create type Server_Status as enum ('enabled', 'disabled');
-create table MQTT_Server
-(
-    ID_Server char(10) primary key,
+create table MQTT_Server (
+    ID_Server serial primary key,
     Url       varchar(255) unique not null,
     Status    Server_Status       not null
 );
 
 create type Designation_Category as enum ('Excellent', 'Fine', 'Moderate', 'Poor', 'Very Poor', 'Severe');
-create table Category
-(
-    ID_Category char(10) primary key,
+create table Category (
+    ID_Category serial primary key,
     Designation Designation_Category not null
 );
 
 create type Units as ENUM ('%', 'mg/m3', 'hPa', 'Celsius', 'ppm', 'ppb', 'aqi', 'mkg/m3', 'n3v/god');
-create table Measured_Unit
-(
-    ID_Measured_Unit char(10) primary key,
+create table Measured_Unit (
+    ID_Measured_Unit serial primary key,
     Title            varchar(255) not null,
     Unit             Units        not null
 );
 
-create table Optimal_Value
-(
-    ID_Category      char(10) not null,
-    ID_Measured_Unit char(10) not null,
+create table Optimal_Value (
+    ID_Category      serial not null,
+    ID_Measured_Unit serial not null,
     Bottom_Border    smallint,
     Upper_Border     smallint,
 
@@ -32,46 +28,50 @@ create table Optimal_Value
     foreign key (ID_Measured_Unit) references Measured_Unit (ID_Measured_Unit)
 );
 
-create table Station
-(
-    ID_Station    char(10) primary key,
+create table Station (
+    ID_Station    serial primary key,
     City          varchar(50)     not null,
     Name          varchar(250)    not null,
     Status        Server_Status   not null,
     ID_SaveEcoBot char(32) unique not null,
-    ID_Server     char(10),
+    ID_Server     serial,
     Coordinates   point           not null,
 
     foreign key (ID_Server) references MQTT_Server (ID_Server)
 );
 
-create table Measurement
-(
-    ID_Measurement   char(10) primary key,
+create table Measurement (
+    ID_Measurement   serial primary key,
     Time             timestamp default now() not null,
     Value            decimal(12, 4)          not null,
-    ID_Station       char(10)                not null,
-    ID_Measured_Unit char(10)                not null,
+    ID_Station       serial                  not null,
+    ID_Measured_Unit serial                  not null,
 
     foreign key (ID_Station) references Station (ID_Station),
     foreign key (ID_Measured_Unit) references Measured_Unit (ID_Measured_Unit)
 );
 
-create table MQTT_Unit
-(
-    ID_Station       char(10)    not null,
-    ID_Measured_Unit char(10)    not null,
+create table MQTT_Unit (
+    ID_Station       serial      not null,
+    ID_Measured_Unit serial      not null,
     Message          varchar(32) not null,
-    "Order"          char(10),
+    "Order"          serial,
 
     foreign key (ID_Station) references Station (ID_Station),
     foreign key (ID_Measured_Unit) references Measured_Unit (ID_Measured_Unit)
 );
 
-create table Favorite
-(
-    ID_User    char(10),
-    ID_Station char(10) not null,
+create table User_Data (
+    ID_User  serial primary key,
+    Login    char(36),
+    Password char(36)
+);
 
+create table Favorite (
+    ID_User    serial,
+    ID_Station serial,
+
+    primary key (ID_User, ID_Station),
+    foreign key (ID_User) references User_Data (ID_User),
     foreign key (ID_Station) references Station (ID_Station)
 );
