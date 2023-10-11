bff9966840bed0a38c1965ee54293b3b735608a1
Pavlo Karas <kar.pavel123@gmail.com>
2023-10-11 18:07:40 +0300

    feat: add new columns to the template of the connected stations list

--- ui/connected_stations.go
+++ ui/connected_stations.go
@@ -8,21 +8,17 @@ import (
 )
 
 type ConnectedStation struct {
-	IdStation        uint
-	City             string
-	Name             string
-	FirstMeasured    string
-	LastMeasurements string
+	IdStation       uint
+	City            string
+	Name            string
+	EnabledFrom     string
+	MeasuredTitle   string
+	MeasuredUnit    string
+	LastMeasurement string
 }
 
 func (app *App) connected_stations(w http.ResponseWriter, r *http.Request) {
-	connected_stations_query, err := app.DB.Query(`
-  select station.id_station, station.city, station.name, mv_first_measurements.first_measured, mv_last_measurements.last_measurements
-  from station
-  left join mv_first_measurements on station.id_station = mv_first_measurements.id_station
-  left join mv_last_measurements on station.id_station = mv_last_measurements.id_station
-  where status = 'enabled'
-  order by station.id_station;`)
+	connected_stations_query, err := app.DB.Query("select * from connected_stations;")
 
 	if err != nil {
 		fmt.Fprintf(w, "Error due selecting connected stations: %v", err)
@@ -35,7 +31,7 @@ func (app *App) connected_stations(w http.ResponseWriter, r *http.Request) {
 	for connected_stations_query.Next() {
 		var ct ConnectedStation
 
-		if err := connected_stations_query.Scan(&ct.IdStation, &ct.City, &ct.Name, &ct.FirstMeasured, &ct.LastMeasurements); err != nil {
+		if err := connected_stations_query.Scan(&ct.IdStation, &ct.City, &ct.Name, &ct.EnabledFrom, &ct.MeasuredTitle, &ct.MeasuredUnit, &ct.LastMeasurement); err != nil {
 			fmt.Fprintf(w, "Error due to scanning a table: %v", err)
 			log.Println(err)
 			return
