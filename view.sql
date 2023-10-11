59bbe15f90372ed553a667f8252f19ffc72e7b04
Pavlo Karas <kar.pavel123@gmail.com>
2023-10-11 17:43:36 +0300

    refactor: add measured units to the connected station list

--- view.sql
+++ view.sql
@@ -1,15 +1,39 @@
 create index idx_measurement_station_time on measurement (id_station, time);
 
 create materialized view mv_first_measurements as
-select distinct id_station,
-                first_value(time) over (w_asc) as first_measured
-from measurement
+select distinct on (id_station, m.id_measured_unit) id_station,
+                                                    mu.id_measured_unit,
+                                                    first_value(time) over (w_asc) as first_measured
+from measurement m
+         left join measured_unit mu on mu.id_measured_unit = m.id_measured_unit
 window w_asc as (partition by id_station order by time)
 order by id_station;
 
 create materialized view mv_last_measurements as
-select distinct id_station,
-                first_value(value) over (w_desc) as last_measurements
-from measurement
-window w_desc as (partition by id_station order by time desc)
+with last_measurements as (select distinct on (id_measured_unit, id_station) id_station, id_measured_unit, value
+                           from measurement
+                           order by id_measured_unit, id_station, time)
+
+select lm.id_station,
+       m.id_measured_unit,
+       m.title,
+       m.unit,
+       lm.value
+from measured_unit m
+         inner join last_measurements lm on m.id_measured_unit = lm.id_measured_unit
 order by id_station;
+
+create view connected_stations as
+select distinct on (station.id_station, mv_last_measurements.id_measured_unit) station.id_station,
+                                                                               station.city,
+                                                                               station.name,
+                                                                               mv_first_measurements.first_measured enbled_from,
+                                                                               mv_last_measurements.title           measured_title,
+                                                                               mv_last_measurements.unit            measured_unit,
+                                                                               mv_last_measurements.value           last_measurement
+from station
+         inner join mv_first_measurements on station.id_station = mv_first_measurements.id_station
+         join mv_last_measurements
+              on mv_first_measurements.id_station = mv_last_measurements.id_station
+where status = 'enabled'
+order by station.id_station;
